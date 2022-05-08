c4ec36f62e28ec5b3250ed29c6a1ae3271952a44
Michael <mcjoy465@gmail.com>
2022-05-08 02:25:21 -0400

    version update

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -41,6 +41,18 @@ public class PlayerController : MonoBehaviour
 	}
 
 	private void CheckInput() {
+		// Gravity + Jump force
+		if (characterController.isGrounded) {
+			velocity = 0;
+		} else {
+			velocity -= Gravity * Time.deltaTime;
+			characterController.Move(new Vector3(0, velocity, 0));
+		}
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			velocity = jumpForce;
+		}
+		characterController.Move(new Vector3(0, velocity, 0) * Time.deltaTime);
+
 		// Movement
 		float currentMovementSpeed = MovementSpeed;
 		if (Input.GetKey(KeyCode.LeftShift)) {
@@ -53,14 +65,6 @@ public class PlayerController : MonoBehaviour
 		float vertical = Input.GetAxis("Vertical") * currentMovementSpeed;
 		characterController.Move((transform.right * horizontal + transform.forward * vertical) * Time.deltaTime);
 
-		// Gravity force
-		if (characterController.isGrounded) {
-			velocity = 0;
-		} else {
-			velocity -= Gravity * Time.deltaTime;
-			characterController.Move(new Vector3(0, velocity, 0));
-		}
-
 		// Camera movement
 		float mouseX = Input.GetAxis("Mouse X") * horizontalSpeed;
 		float mouseY = Input.GetAxis("Mouse Y") * verticalSpeed;
@@ -68,11 +72,5 @@ public class PlayerController : MonoBehaviour
 		xRotation -= mouseY;
 		xRotation = Mathf.Clamp(-mouseY, -90, 90);
 		cam.transform.Rotate(xRotation, 0.0f, 0.0f);
-
-		// Jumping
-		if (Input.GetKeyDown(KeyCode.Space)) {
-			velocity = jumpForce;
-    		characterController.Move(new Vector3(0, velocity, 0) * Time.deltaTime);
-		}
 	}
 }
